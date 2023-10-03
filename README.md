d37ca6b52fd25cfed7c346c30d6e7bcfff6d9669
Meister1593 <leruop@gmail.com>
2023-10-04 02:07:44 +0500

    Refactor for Resonite

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # SaveScreenshotToDifferentDirectory
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/).
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/).
 
 Created due to severe issues with Steam saving screenshots unreliably on Linux, but should work on Windows as well. 
 
 ## Usage
-1. Install [NeosModLoader](https://github.com/neos-modding-group/NeosModLoader).
-2. Place [SaveScreenshotToDifferentDirectory.dll](https://github.com/Meister1593/SaveScreenshotToDifferentDirectory/releases/latest) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR` on windows or `$HOME/.steam/steam/steamapps/common/NeosVR` for a default install on linux.
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+2. Place [SaveScreenshotToDifferentDirectory.dll](https://github.com/Meister1593/SaveScreenshotToDifferentDirectory/releases/latest) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite` on windows or `$HOME/.steam/steam/steamapps/common/Resonite` for a default install on linux.
 3. Start the game. Change default folder to something you would like in NeosModLoader settings under mod tab.
 
-Do note that this might not work with Standalone client, as it hooks up to Steam part of the NeosVR codebase.
+Do note that this might not work with Standalone client, as it hooks up to Steam part of the Resonite codebase.
 
 If you use Proton on Linux, any type of path will work as long as it can reach that folder: 
  * with forward slashes only
  * backward slashes only 
- * full Windows-like path)
+ * full Windows-like path
--- SaveScreenshotToDifferentDirectory/SaveScreenshotToDifferentDirectory.cs
+++ SaveScreenshotToDifferentDirectory/SaveScreenshotToDifferentDirectory.cs
@@ -1,15 +1,19 @@
 ﻿using HarmonyLib;
-using NeosModLoader;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using FrooxEngine;
+using ResoniteModLoader;
 using Steamworks;
 
 namespace SaveScreenshotToDifferentDirectory
 {
-    public class SaveScreenshotToDifferentDirectory : NeosMod
+    public class SaveScreenshotToDifferentDirectory : ResoniteMod
     {
-        internal const string VERSION = "1.0.0";
-
+        internal const string VERSION = "2.0.0";
         public override string Name => "SaveScreenshotToDifferentDirectory";
         public override string Author => "Meister1593";
         public override string Version => VERSION;
@@ -17,33 +21,86 @@ namespace SaveScreenshotToDifferentDirectory
 
         public override void OnEngineInit()
         {
-            Config = GetConfiguration();
+            _config = GetConfiguration();
             new Harmony("net.meister1593.SaveScreenshotToDifferentDirectory").PatchAll();
         }
 
-        private static ModConfiguration Config;
+        private static ModConfiguration _config;
 
         [AutoRegisterConfigKey]
-        public static ModConfigurationKey<string> ScreenshotPath =
+        private static readonly ModConfigurationKey<string> ScreenshotPath =
             new("Path to screenshots", "Path to screenshots folder in OS", () => "");
 
         [HarmonyPatch(typeof(SteamScreenshots), nameof(SteamScreenshots.AddVRScreenshotToLibrary))]
         public class SteamScreenshots_AddVRScreenshotToLibrary_Patch
         {
-            static void Prefix(EVRScreenshotType eType,
+            private static void Prefix(EVRScreenshotType eType,
                 string pchFilename,
                 string pchVRFilename)
             {
-                string dateTime = DateTime.Now.ToString("yyyyMMddHHmmss");
-                string screenshotPath =
-                    $"{Config.GetValue(ScreenshotPath)}/{dateTime}_1.jpg"; // Steam screenshot name format
-                if (string.IsNullOrEmpty(Config.GetValue(ScreenshotPath)))
+                var dateTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+                var screenshotPath =
+                    $"{_config.GetValue(ScreenshotPath)}/{dateTime}_1.jpg"; // Steam screenshot name format
+                if (string.IsNullOrEmpty(_config.GetValue(ScreenshotPath)))
                 {
                     return;
                 }
 
-                Warn(screenshotPath);
+                Msg($"Path to screenshot saved: {screenshotPath}");
                 File.Copy(pchFilename, screenshotPath);
+            }
+        }
+
+        [HarmonyPatch(typeof(PhotoCaptureManager))]
+        [HarmonyPatch("TakePhoto")]
+        public static class PhotoCaptureManager_TakePhoto_Patch
+        {
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var foundMassUsageMethod = false;
+                var startIndex = -1;
+                var endIndex = -1;
+
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldarg_0)
+                    {
+                        if (foundMassUsageMethod)
+                        {
+                            Msg("END " + i);
+
+                            endIndex = i; // include current 'ret'
+                            break;
+                        }
+
+                        Msg("START " + (i + 1));
+
+                        startIndex = i + 1; // exclude current 'call'
+
+                        for (var j = startIndex; j < codes.Count; j++)
+                        {
+                            if (codes[j].opcode == OpCodes.Call)
+                                break;
+                            var strOperand = (codes[j].operand as MethodInfo)?.Name;
+                            if (strOperand == "PlayCaptureSound")
+                            {
+                                foundMassUsageMethod = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (startIndex > -1 && endIndex > -1)
+                {
+                    // we cannot remove the first code of our range since some jump actually jumps to
+                    // it, so we replace it with a no-op instead of fixing that jump (easier).
+                    codes[startIndex].opcode = OpCodes.Nop;
+                    codes.RemoveRange(startIndex + 1, endIndex - startIndex - 1);
+                }
+
+                return codes.AsEnumerable();
             }
         }
     }
